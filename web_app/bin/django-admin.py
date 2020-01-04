29e348d0a08cb390e6b452035d0b64b36671126d
Ankit Thakur <ankit@wealthtech.in>
2020-01-04 13:23:15 +0530

    removed venv files

--- web_app/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/ankitthakur/PycharmProjects/ankit-portfolio/web_app/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
